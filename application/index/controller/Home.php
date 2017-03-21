e5dae508d239cebf6962b0272cf1d6d8f6b1fb6e
xiaochongMK <mk_note@163.com>
2017-03-21 21:30:38 +0800

    首页和登录页面样式的添加

---
+++ application/index/controller/Home.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use app\index\model\UserModel;
+use think\Request;
+
+class Home extends Controller
+{
+    public function index(){
+       
+        return view('index');
+
+    }
+
+	public function _empty($name)
+    {
+        //把所有空的操作解析到showfan方法
+        return $this->showFun($name);
+    }
+
+    //注意 本身是 protected 方法
+    protected function showFun($name)
+    {
+         return view($name);
+    }
+}
