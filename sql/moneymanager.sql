45459b62ed492a307a0dfd20fcf939e790fe7e6a
xiachongMK <mk_note@163.com>
2017-03-24 22:32:26 +0800

    修改数据库

--- sql/moneymanager.sql
+++ sql/moneymanager.sql
@@ -1,19 +1,97 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
+Source Server         : php
 Source Server Version : 50617
-Source Host           : localhost:3306
-Source Database       : moneymanager
+Source Host           : 127.0.0.1:3306
+Source Database       : moneymanger
 
 Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-03-24 18:48:59
+Date: 2017-03-24 22:30:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for bank
+-- ----------------------------
+DROP TABLE IF EXISTS `bank`;
+CREATE TABLE `bank` (
+  `bank_id` int(11) DEFAULT NULL,
+  `bank_name` varchar(255) DEFAULT NULL,
+  `bank_remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of bank
+-- ----------------------------
+INSERT INTO `bank` VALUES ('1', '中国银行', null);
+INSERT INTO `bank` VALUES ('2', '中国建设银行', null);
+INSERT INTO `bank` VALUES ('3', '中国农业银行', null);
+INSERT INTO `bank` VALUES ('4', '中国邮政储蓄银行', null);
+INSERT INTO `bank` VALUES ('5', '招商银行', null);
+INSERT INTO `bank` VALUES ('6', '中国光大银行', null);
+INSERT INTO `bank` VALUES ('7', '中国工商银行', null);
+INSERT INTO `bank` VALUES ('8', '浦发银行', null);
+INSERT INTO `bank` VALUES ('9', '交通银行', null);
+INSERT INTO `bank` VALUES ('10', '广发银行', null);
+INSERT INTO `bank` VALUES ('11', '平安银行', null);
+INSERT INTO `bank` VALUES ('12', '民生银行', null);
+INSERT INTO `bank` VALUES ('13', '其他银行', null);
+
+-- ----------------------------
+-- Table structure for budget
+-- ----------------------------
+DROP TABLE IF EXISTS `budget`;
+CREATE TABLE `budget` (
+  `bid` int(11) NOT NULL,
+  `b_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `b_term` varchar(255) DEFAULT NULL,
+  `b_money` varchar(255) DEFAULT NULL,
+  `b_remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of budget
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for card
+-- ----------------------------
+DROP TABLE IF EXISTS `card`;
+CREATE TABLE `card` (
+  `cardid` int(11) NOT NULL,
+  `c_bank` varchar(255) DEFAULT NULL,
+  `c_limit` varchar(255) DEFAULT NULL,
+  `zd_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `hk_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `c_days` int(255) DEFAULT NULL,
+  `deadline` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of card
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for card_account
+-- ----------------------------
+DROP TABLE IF EXISTS `card_account`;
+CREATE TABLE `card_account` (
+  `caid` int(11) NOT NULL,
+  `ca_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `ca_operation` varchar(255) DEFAULT NULL,
+  `ca_sort` varchar(255) DEFAULT NULL,
+  `ca_money` varchar(255) DEFAULT NULL,
+  `ca_remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of card_account
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for cost
@@ -34,28 +112,62 @@ CREATE TABLE `cost` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for c_sort
+-- Table structure for csort
 -- ----------------------------
-DROP TABLE IF EXISTS `c_sort`;
-CREATE TABLE `c_sort` (
+DROP TABLE IF EXISTS `csort`;
+CREATE TABLE `csort` (
   `csid` int(11) NOT NULL,
   `c_sort` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 -- ----------------------------
--- Records of c_sort
+-- Records of csort
 -- ----------------------------
-INSERT INTO `c_sort` VALUES ('1', '衣服饰品');
-INSERT INTO `c_sort` VALUES ('2', '食品酒水');
-INSERT INTO `c_sort` VALUES ('3', '居家物业');
-INSERT INTO `c_sort` VALUES ('4', '行车交通');
-INSERT INTO `c_sort` VALUES ('5', '交流通讯');
-INSERT INTO `c_sort` VALUES ('6', '休闲娱乐');
-INSERT INTO `c_sort` VALUES ('7', '学习进修');
-INSERT INTO `c_sort` VALUES ('8', '人情往来');
-INSERT INTO `c_sort` VALUES ('9', '医疗保健');
-INSERT INTO `c_sort` VALUES ('10', '金融保险');
-INSERT INTO `c_sort` VALUES ('11', '其他款项');
+INSERT INTO `csort` VALUES ('1', '衣服饰品');
+INSERT INTO `csort` VALUES ('2', '食品酒水');
+INSERT INTO `csort` VALUES ('3', '居家物业');
+INSERT INTO `csort` VALUES ('4', '行车交通');
+INSERT INTO `csort` VALUES ('5', '交流通讯');
+INSERT INTO `csort` VALUES ('6', '休闲娱乐');
+INSERT INTO `csort` VALUES ('7', '学习进修');
+INSERT INTO `csort` VALUES ('8', '人情往来');
+INSERT INTO `csort` VALUES ('9', '医疗保健');
+INSERT INTO `csort` VALUES ('10', '金融保险');
+INSERT INTO `csort` VALUES ('11', '其他款项');
+
+-- ----------------------------
+-- Table structure for debtee
+-- ----------------------------
+DROP TABLE IF EXISTS `debtee`;
+CREATE TABLE `debtee` (
+  `eid` int(11) NOT NULL,
+  `e_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `e_debtee` varchar(255) DEFAULT NULL,
+  `e_debtor` varchar(255) DEFAULT NULL,
+  `e_money` varchar(255) DEFAULT NULL,
+  `e_remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of debtee
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for debtor
+-- ----------------------------
+DROP TABLE IF EXISTS `debtor`;
+CREATE TABLE `debtor` (
+  `rid` int(11) NOT NULL,
+  `r_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `r_debtee` varchar(255) DEFAULT NULL,
+  `r_debtor` varchar(255) DEFAULT NULL,
+  `r_money` varchar(255) DEFAULT NULL,
+  `r_remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of debtor
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for income
@@ -76,17 +188,39 @@ CREATE TABLE `income` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for i_sort
+-- Table structure for isort
 -- ----------------------------
-DROP TABLE IF EXISTS `i_sort`;
-CREATE TABLE `i_sort` (
+DROP TABLE IF EXISTS `isort`;
+CREATE TABLE `isort` (
   `isid` int(11) NOT NULL,
-  `work` varchar(255) DEFAULT NULL,
-  `i_other` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+  `i_sort` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of i_sort
+-- Records of isort
+-- ----------------------------
+INSERT INTO `isort` VALUES ('1', '职业收入');
+INSERT INTO `isort` VALUES ('2', '其他收入');
+
+-- ----------------------------
+-- Table structure for loan
+-- ----------------------------
+DROP TABLE IF EXISTS `loan`;
+CREATE TABLE `loan` (
+  `lid` int(11) NOT NULL,
+  `l_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `l_name` varchar(255) DEFAULT NULL,
+  `l_sort` varchar(255) DEFAULT NULL,
+  `l_money` varchar(255) DEFAULT NULL,
+  `l_lilv` varchar(255) DEFAULT NULL,
+  `l_method` varchar(255) DEFAULT NULL,
+  `l_bank` varchar(255) DEFAULT NULL,
+  `l_remark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of loan
 -- ----------------------------
 
 -- ----------------------------
@@ -107,6 +241,27 @@ INSERT INTO `member` VALUES ('3', '老婆');
 INSERT INTO `member` VALUES ('4', '子女');
 INSERT INTO `member` VALUES ('5', '父母');
 INSERT INTO `member` VALUES ('6', '家庭公用');
+
+-- ----------------------------
+-- Table structure for shares
+-- ----------------------------
+DROP TABLE IF EXISTS `shares`;
+CREATE TABLE `shares` (
+  `sid` int(11) DEFAULT NULL,
+  `s_code` varchar(255) DEFAULT NULL,
+  `s_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `s_price` varchar(255) DEFAULT NULL,
+  `s_num` varchar(255) DEFAULT NULL,
+  `s_yongjin` varchar(255) DEFAULT NULL COMMENT '佣金',
+  `s_yhs` varchar(255) DEFAULT NULL COMMENT '印花税',
+  `s_ghs` varchar(255) DEFAULT NULL COMMENT '过户税',
+  `s_money` varchar(255) DEFAULT NULL,
+  `s_remark` varchar(255) DEFAULT NULL COMMENT '备注'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of shares
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
